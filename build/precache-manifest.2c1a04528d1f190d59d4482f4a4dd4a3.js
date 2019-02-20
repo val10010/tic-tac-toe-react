66d51d6efbaab23c8cf916cb78314e3ae465f35c
Valentyn <valantine10010@gmail.com>
2019-02-20 09:39:38 +0200

    add build

---
+++ build/precache-manifest.2c1a04528d1f190d59d4482f4a4dd4a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bae1d4531f1ba075701a",
+    "url": "/static/js/main.bae1d453.chunk.js"
+  },
+  {
+    "revision": "e3883387d6005201448b",
+    "url": "/static/js/2.e3883387.chunk.js"
+  },
+  {
+    "revision": "bae1d4531f1ba075701a",
+    "url": "/static/css/main.b8886739.chunk.css"
+  },
+  {
+    "revision": "42e330426bff21f616a0af72bd5d3e3d",
+    "url": "/index.html"
+  }
+];
